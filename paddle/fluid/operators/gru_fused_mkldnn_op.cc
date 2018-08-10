aa11d48ec3b1bdc710b219e4a608ab1cd70583d9
Brian Liu <brian.liu@intel.com>
2018-08-10 12:49:58 +0800

    Add code to convert MKLDNN RNN output to Hidden tensor

--- paddle/fluid/operators/gru_fused_mkldnn_op.cc
+++ paddle/fluid/operators/gru_fused_mkldnn_op.cc
@@ -79,12 +79,12 @@ class GRUFusedMKLDNNKernel : public framework::OpKernel<T> {
     int D = is_bidir ? 2 : 1;              // 2 if bidirection
     int L = ctx.Attr<int>("stack_level");  // number of layers
     int G = 3;                             // number of gates, 3 for gru
-    //int TSum = x_dims[0];                  // total time steps in mini-batch
+    int TSum = x_dims[0];                  // total time steps in mini-batch
     int I = x_dims[1];                     // input feature size
     int C = weight_h_dims[0];              // hidden state size 
 
     // Allocate memory for output
-    hidden->mutable_data<T>(ctx.GetPlace());
+    T* hidden_data = static_cast<T*>(hidden->mutable_data<T>(ctx.GetPlace()));
 
     // Input x is a batch of sequence each with variable length (time step)
     // Go through all sequences and get start position and len for each seq
@@ -114,23 +114,100 @@ class GRUFusedMKLDNNKernel : public framework::OpKernel<T> {
     //   TMax: max lengh of all sequences in this batch
     //      N: batch size
     //      I: input feature size
+    // example: input x = {s1(w11,w12,w13,w14),
+    //                     s2(w21,w22,w23,w24,w25),
+    //                     s3(w31,w32,w33)}
+    //           s2  - the 2nd sentence in this batch
+    //           w24 - the 4th word in the 2nd sentence
+    //
+    // In this example, TSum=4+5+3=12; TMax=5 (len of s2); N=3
+    //
     // Two operations need here
     //  1. Unpack: each sequence will be extended to TMax(*I)
     //  2. Transpose: T1 of all sequences firstly, then T2, T3, ...
+    //
+    //  The input x will be reordered by sentence length firstly
+    //           reorderd X = {s2(w21,w22,w23,w24,w25),
+    //                         s1(w11,w12,w13,w14),
+    //                         s3(w31,w32,w33)}
+    //  Then reordered x will be unpack/tranpose to batch_x with [TMax,N,I]
+    //           batch_x = {(w21,w11,w31), <-- First word of all sentences
+    //                      (w22,w12,w32),
+    //                      (w23,w13,w33),
+    //                      (w24,w14,  0), <-- 0 padding for short sentence
+    //                      (w25,  0,  0)}
+    // 
+    //  batch_x will be feeded into MKLDNN RNN primitive as input
+    //  1st batch (w21,w11,w31) in batch_x means 1st time step data feeding into RNN.
+    //  !!! Note !!!
+    //  1st "batch" in batch_x (w21,w11,w31) != 1st "batch" in x (w11,w12,w13,w14)
+    //
+    //  batch_lods (3 level LoD) will be used to save above order info in batch_x
+    //     batch_lods[0] = {0, 3,  6, 9, 11, 12}
+    //     batch_lods[1] = {4, 0,  9,
+    //                      5, 1, 10,
+    //                      6, 2, 11,
+    //                      7, 3,
+    //                      8}
+    //     batch_lods[2] = {1, 0, 2} 
+    // batch_lods[0][2] = 6 means the 3rd batch in batch_x (w23,w13,w33) is at position
+    // 6, i.e. after two batches (w21,w11,w31) and (w22,w12,w32)
+    // batch_lods[1][7] = 2 means the 3rd (2+1, 0 based) word "w13"  in original x
+    // is now in position 7 of new batch_x
+    // batch_lods[2] is the order of original setence, i.e. {s2, s1, s3}
+
+    // batch_lod is a 3 level LoD
+    paddle::framework::LoD batch_lods;
+    batch_lods.emplace_back(std::vector<size_t>{0});
+    batch_lods.emplace_back(std::vector<size_t>{0});
+    batch_lods.emplace_back(std::vector<size_t>{0});
+    // batch_lods[0] is the start position of each time step in batch_x
+    batch_lods[0].resize(static_cast<size_t>(TMax+1));
+    // batch_lods[1] is the raw index in input x
+    batch_lods[1].resize(static_cast<size_t>(TSum));
+    // batch_lods[2] is the sort order
+    batch_lods[2].resize(seq_info.size());
+
+    std::vector<int> batch_lens;
+    batch_lens.resize(TMax);
+    
+    // compute batch_lod and batch_lens which will be used in output
+    size_t* batch_starts = batch_lods[0].data();
+    size_t* seq2batch_idx = batch_lods[1].data();
+    size_t* seq_order = batch_lods[2].data();
+    batch_starts[0] = 0;
+    for (int t = 0; t < TMax; t++) { // each time step
+      auto batch_id = static_cast<int>(batch_starts[t]);
+      int i;
+      for (i = 0; i < N; ++i) { // each sentence
+        int seq_len = seq_info[i].length;
+        int start = seq_info[i].start;
+        if (t < seq_len) {
+          seq2batch_idx[batch_id] = start + t;
+          batch_id++;
+        } else {
+          break;
+        }
+      }
+      batch_starts[t + 1] = static_cast<size_t>(batch_id);
+      batch_lens[t] = i + 1;
+    }
+    for (size_t i = 0; i < seq_info.size(); ++i) {
+      seq_order[i] = seq_info[i].seq_idx;
+    }
+
+    // reorder input x to batch_x 
     const T* x_data = input->data<T>();
     std::vector<T> batch_x(TMax * N * I, 0); // unpack with zero padding
     T* batch_x_data = batch_x.data();
-#pragma omp parallel for simd
     for (int s = 0; s < N; s++) { // for each sequence
       // get source address of this sequence
       auto ss = x_data + seq_info[s].start * I;
       for (int t = 0; t < seq_info[s].length; t++) { // for each time step
-	// get src/target address for this time step in this sequence
-	auto sst = ss + t * I;
-	auto dst = batch_x_data + t * N * I + s;
-        for (int c = 0; c < I; c++) {
-          *(dst + c) = *(sst + c); // memcpy instead?
-	}
+        // get src/target address for this time step in this sequence
+        auto sst = ss + t * I;
+        auto dst = batch_x_data + (t * N + s) * I;
+	memcpy(dst, sst, I * sizeof(T));
       }
     }
 
@@ -234,8 +311,24 @@ class GRUFusedMKLDNNKernel : public framework::OpKernel<T> {
     std::vector<mkldnn::primitive> pipeline = {forward_op};
     mkldnn::stream(mkldnn::stream::kind::eager).submit(pipeline).wait();
 
-    // reorder batch hidden memory (TMax,N,C) back to a LoD output (TSum,C)
+    // reorder batch hidden data (TMax,N,C) back to hidden data (TSum,C)
+    T *batch_hidden_data = static_cast<T*>(batch_hidden_memory.get_data_handle());
+    int offset = 0;
+    for (int t = 0; t < TMax; t++) { // for each time step
+      int i;
+      for (i = 0; i < batch_lens[i]; i++) { // for each word at specified time step
+	memcpy(hidden_data + offset * C,
+	       batch_hidden_data + (t * N + i) * C,
+	       C * sizeof(T));
+      }
+      offset += batch_lens[i];
+    }
+    PADDLE_ENFORCE_EQ(offset, TSum,
+		      "Hidden output should have same length as input x");
 
+    // Need set LoD to output tensor
+    hidden->set_lod(batch_lods);
+    
     hidden->set_layout(DataLayout::kMKLDNN);
     hidden->set_format((const mkldnn::memory::format)batch_hidden_memory.get_primitive_desc().desc().data.format);
     //hidden->set_format(GetMKLDNNFormat(hidden_state_memory));
