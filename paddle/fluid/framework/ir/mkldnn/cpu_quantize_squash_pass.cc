04d7670a87525787602c62f3edcb93ae1081abf6
lidanqing <danqing.li@intel.com>
2019-12-03 16:32:56 +0100

    Add reshape_transpose_scale pass in separate files

--- paddle/fluid/framework/ir/mkldnn/cpu_quantize_squash_pass.cc
+++ paddle/fluid/framework/ir/mkldnn/cpu_quantize_squash_pass.cc
@@ -52,7 +52,7 @@ void CPUQuantizeSquashPass::FindNodesToKeep(
 void CPUQuantizeSquashPass::ReshapeTransposeScaleSquash(
     Graph* graph) const{
     GraphPatternDetector gpd;
-    patterns::Reshape2Transpose2Scale reshape_transpose_scale_pattern{gpd.mutable_pattern(), "reshape_transpose_scale"};  
+    patterns::ReshapeTransposeScale reshape_transpose_scale_pattern{gpd.mutable_pattern(), "reshape_transpose_scale"};  
     reshape_transpose_scale_pattern();
 
     int found_reshape_transpose_scale_count = 0;
