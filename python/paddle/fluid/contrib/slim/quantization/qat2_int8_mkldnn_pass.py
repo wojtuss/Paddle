1a319508d195ce09f2534c0ee40471b1654b1959
Wojciech Uss <wojciech.uss@intel.com>
2020-05-26 12:36:10 +0200

    fix

--- python/paddle/fluid/contrib/slim/quantization/qat2_int8_mkldnn_pass.py
+++ python/paddle/fluid/contrib/slim/quantization/qat2_int8_mkldnn_pass.py
@@ -338,15 +338,18 @@ class Qat2Int8MkldnnPass(object):
         graph = self._apply_pass(graph, 'depthwise_conv_mkldnn_pass')
         graph = self._apply_pass(graph, 'conv_bn_fuse_pass')
         graph = self._apply_pass(graph, 'conv_eltwiseadd_bn_fuse_pass')
-        graph = self._apply_pass(graph, 'conv_bias_mkldnn_fuse_pass')
-        graph = self._apply_pass(graph, 'conv_elementwise_add_mkldnn_fuse_pass')
-        graph = self._apply_pass(graph, 'conv_relu_mkldnn_fuse_pass')
-        graph = self._apply_pass(graph, 'conv_relu6_mkldnn_fuse_pass')
-        graph = self._apply_pass(graph, 'fc_fuse_pass',
-                                 ['use_gpu', 'use_fc_padding'], [False, False])
-        if self._is_fc_quantized(graph):
-            graph = self._apply_pass(graph, 'fc_mkldnn_pass')
-        graph = self._apply_pass(graph, 'matmul_transpose_reshape_fuse_pass')
+        graph = self._apply_pass(graph, 'conv_transpose_bn_fuse_pass')
+        graph = self._apply_pass(graph,
+                                 'conv_transpose_eltwiseadd_bn_fuse_pass')
+        #  graph = self._apply_pass(graph, 'conv_bias_mkldnn_fuse_pass')
+        #  graph = self._apply_pass(graph, 'conv_elementwise_add_mkldnn_fuse_pass')
+        #  graph = self._apply_pass(graph, 'conv_relu_mkldnn_fuse_pass')
+        #  graph = self._apply_pass(graph, 'conv_relu6_mkldnn_fuse_pass')
+        #  graph = self._apply_pass(graph, 'fc_fuse_pass',
+        #  ['use_gpu', 'use_fc_padding'], [False, False])
+        #  if self._is_fc_quantized(graph):
+        #  graph = self._apply_pass(graph, 'fc_mkldnn_pass')
+        #  graph = self._apply_pass(graph, 'matmul_transpose_reshape_fuse_pass')
         return graph
 
     def _apply_pass(self, graph, pass_name, attrs=None, attr_values=None):
@@ -371,7 +374,7 @@ class Qat2Int8MkldnnPass(object):
         # remove dropout ops
         graph = self._apply_pass(graph, 'simplify_with_basic_ops_pass')
         # make some MKL-DNN ops working inplace
-        graph = self._apply_pass(graph, 'mkldnn_inplace_pass')
+        #  graph = self._apply_pass(graph, 'mkldnn_inplace_pass')
         return graph
 
     def _cleanup(self, graph):
